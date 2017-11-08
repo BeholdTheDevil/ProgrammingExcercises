acc0bfec858cb887eeabbfa677b1cac52772fb18
BeholdTheDevil <antdeg@outlook.com>
2017-11-08 14:58:24 +0100

    Relations Excercise

---
+++ src/Relations/MyCircle.java
@@ -0,0 +1,87 @@
+package Relations;
+
+/**
+ * Created by anton on 2017-11-08.
+ */
+public class MyCircle {
+    private MyPoint center = new MyPoint(0, 0);
+    private int radius = 1;
+
+    public MyCircle() {
+    }
+
+    public MyCircle(int x, int y, int radius) {
+        center = new MyPoint(x, y);
+        this.radius = radius;
+    }
+
+    public MyCircle(MyPoint center, int radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public MyPoint getCenter() {
+        return center;
+    }
+
+    public void setCenter(MyPoint center) {
+        this.center = center;
+    }
+
+    public int getCenterX() {
+        return center.getX();
+    }
+
+    public void setCenterX(int x) {
+        center.setX(x);
+    }
+
+    public int getCenterY() {
+        return center.getY();
+    }
+
+    public void setCenterY(int y) {
+        center.setY(y);
+    }
+
+    public int[] getCenterXY() {
+        return new int[] {center.getX(), center.getY()};
+    }
+
+    public void setCenterXY(int x, int y) {
+        center.setX(x);
+        center.setY(y);
+    }
+
+    public String toString() {
+        return new String("MyCircle[radius=" + radius + ", center=" + center.toString() + "]");
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getCircumference() {
+        return 2 * radius * Math.PI;
+    }
+
+    public double distance(MyCircle another) {
+        return center.distance(another.center);
+    }
+
+    public double distance(int x, int y) {
+        return center.distance(x, y);
+    }
+
+    public double distance() {
+        return center.distance();
+    }
+}
